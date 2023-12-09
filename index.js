004b3afe49f67123e1071025506e0d4d63f73a5f
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-12-09 19:28:19 -0300

    Iniciando o jest

--- index.js
+++ index.js
@@ -2,37 +2,7 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verfiqueSe = (valor) => {
-    const assercoes = {
-        ehExatamenteIgualA(esperado) {
-            if (valor !== esperado) {
-                throw {};
-            }
-        }
-    };
-    return assercoes;
+export {
+    somaHorasExtras,
+    calculaDescontos,
 };
-
-const teste = (titulo, funcaoDeTeste) => {
-    try {
-        funcaoDeTeste();
-        console.log(`${titulo} passou!`);
-    }
-    catch {
-        console.error(`${titulo} não passou!`);
-    }
-};
-
-teste('somaHorasExtras', () => {
-    const esperado = 2500;
-    const retornado = somaHorasExtras(2000, 500);
-
-    verfiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
-
-teste('calculaDesconto', () => {
-    const esperado = 2300;
-    const retornado = calculaDescontos(2500, 200);
-
-    verfiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
