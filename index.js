5c3221b322587bc4da2eb22fc1cd77317b49f34e
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-12-09 17:04:59 -0300

    Asserções

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const somaHorasExtras = (salario, valorHorasExtras) => {
-    return salario + valorHorasExtras;
-}
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2000, 500))
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verfiqueSe = (valor) => {
+    const assercoes = {
+        ehExatamenteIgualA(esperado) {
+            if (valor !== esperado) {
+                throw {};
+            }
+        }
+    };
+    return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+    try {
+        funcaoDeTeste();
+        console.log(`${titulo} passou!`);
+    }
+    catch {
+        console.error(`${titulo} não passou!`);
+    }
+};
+
+teste('somaHorasExtras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
+
+    verfiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
+
+    verfiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
